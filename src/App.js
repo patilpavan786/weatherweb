703952e089a925614c5397b8f30e8ccfbb2f1c9a
patilpavan786 <pavan.p.b.pavan@gmail.com>
2023-02-15 21:28:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Home from './Home';
+import { RecoilRoot } from 'recoil';
+function App() {
+  return (
+    < RecoilRoot >
+   <Home/>
+   </ RecoilRoot >
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,55 @@
+import React,{useState} from "react";
+import "./style.css";
+import TornadoIcon from "@mui/icons-material/Tornado";
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import Search from "./card/Search";
+import WeatherCard from "./card/WeatherCard";
+function Home() {
+
+  const [value, setValue] = useState("");
+    const [city, setCity] = useState("");
+    const[temp,setTemp]=useState("")
+  function handleChange(e) {
+      setValue(e.target.value);
+    fetchApi(); 
+  }
+      const fetchApi = async () => {
+    
+          fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=b24982bca818f3dea1c76357d4773f19`)
+              .then(response => response.json())
+              .then(data => {
+                  console.log(data); 
+                  setCity(data.name)
+                  setTemp((data.main.temp-273.15).toFixed(1))
+                  console.log(temp)
+                  
+              })
+             
+      }
+
+  return (
+    <div className="weaterWrapper">
+      <div className="header">
+        <h1>
+   
+          <WbSunnyIcon style={{ fontSize: "40px", marginRight: "1rem",color:"yellow"}} />
+          WeatherApp
+        </h1>
+      </div>
+      <div className="SearchFilter">
+        <Search 
+         onChange={(e)=> handleChange(e)}
+         value={value}
+        />
+      </div>
+      <div className="weatherCard">
+        <WeatherCard 
+        city={city}
+        temp={temp}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
